e76dfae8e007448b215856f2e8a8367bc2a25d84
BakryDiarra <bakrydiarra@gmail.com>
2023-02-04 09:19:28 +0000

    Add run_quiz to iterate through questions and multiple choice answer, add time.sleep method to some print from function start

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-"Quiz material"
+"Questions and answers to be imported to run.py "
 
 
 class Question:
@@ -16,8 +16,14 @@ class Question:
 
 
 quiz_questions = [
-    Question("Where is Captain America from?", "b", ["(a) Chicago", "(b) Brooklyn", "(c) Cleveland"]),
-    Question("The shield of Captain America is made of what?", "c", ["(a) Tetranium", "(b) Palladium", "(c) Vibranium"]),
-    Question("What is the metal inside Wolverine?", "b", ["(a) Tetranium", "(b) Adamantium", "(c) Krytasium"]),
-
+    Question("Where is Captain America from", "b", ["(a) Chicago", "(b) Brooklyn", "(c) Cleveland"]),
+    Question("The shield of Captain America is made of what", "c", ["(a) Tetranium", "(b) Palladium", "(c) Vibranium"]),
+    Question("What is the metal inside Wolverine", "b", ["(a) Tetranium", "(b) Adamantium", "(c) Krytasium"]),
+    Question("What was the skin color  of the original Hulk", "c", ["(a) blue", "(b) yellow", "(c) gray" ]),
+    Question("Which country is the birthplace of Black Widow", "c", ["(a) Belarus", "(b) Ukraine", "(c) Russia"]),
+    Question("Who is the brother of Scarlet Witch", "a", ["(a) Quicksilver", "(b) Deathstrike", "(c) Silverburn"]),
+    Question("Who does the Mad Titan sacrifice to acquire the Soul Stone", "b", ["(a) Nebulla", "(b) Gamora", "(c) Ramonda"]),
+    Question("Who is the sister of the Black Panther", "c", ["(a) Nakia", "(b) Sooki", "(c) Shuri"]),
+    Question("What color is the power stone", "a", ["(a) purple", "(b) green", "(c) red"]),
+    Question("Who is the Winter Soldier", "b", ["(a) Hawkeye", "(b) Bucky", "(c) Heimdall"])
 ]
--- run.py
+++ run.py
@@ -2,23 +2,31 @@
 Import libraries needed to the function of this application.
 """
 import time
+from time import sleep
 import colorama
 from colorama import Fore, Style
 colorama.init(autoreset=True)
 from logo import TITLE
+from quiz import Question
+from quiz import quiz_questions
 
 
 def start():
     """
-    Display logo and instructions of the quiz
-    Asking username input to run the quiz
+    Display logo and instructions of the quiz.
+    Asking username input to run the quiz.
     """
     print(TITLE)
     print("Welcome to the superheroes universe!\n")
+    time.sleep(1)
     print("Are you ready to test your knowledge?\n")
+    time.sleep(1)
     print("You have 10 multiple choice questions.\n")
+    time.sleep(1)
     print("Select your answer typing 'a', 'b' or 'c'.\n")
+    time.sleep(1)
     validate_name()
+    run_quiz()
 
 
 def validate_name():
@@ -26,21 +34,37 @@ def validate_name():
     validation of the username to start the game
     """
     while True:
-        name = input("Enter your name to start the quiz. \n")
+        name = input("Enter your name to start the quiz: \n")
         name = name.strip().lower()
         print("")
         if len(name) > 10:
             print(f"""{Fore.RED}Please enter a name with 10 or less character \n""")
         else:
             print(f"Godd luck, {name} !")
+            print("")
             break
 
 
 def run_quiz():
     """
-    Iterate
+    Iterate through the questions list.
+    Iterate through the choice list.
+    Get user input-answer.
+    Implementing score +=1 or +=0
     """
-    pass
+    global useranswer
+    for question in quiz_questions:
+        print(f"{question.item}?")
+        for option in question.choice:
+            print(option)
+        useranswer = input("Enter your answer: \n")
+
+
+def check_answer():
+    """
+    Check user input-answer
+    Implement score
+    """
 
 
 start()
